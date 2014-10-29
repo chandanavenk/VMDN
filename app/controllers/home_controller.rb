5a689beec4cd3d080d1e23a42740d273a69bf80c
CHANDANA <chandana.ven8@yahoo.in>
2014-10-29 03:13:54 -0700

    Alterations made

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -1,4 +1,12 @@
 class HomeController < ApplicationController
-  def index
+  
+    def index
+    @employees = Employee.all
+
+    respond_to do |format|
+      format.html # index.html.erb
+      format.json { render json: @employees }
+    end
+
   end
 end
