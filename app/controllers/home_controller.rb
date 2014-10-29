89723b78a9e4ecf6e8d068a5893499850cfcc3fe
CHANDANA <chandana.ven8@yahoo.in>
2014-10-29 04:38:38 -0700

    changes in link in the side bar

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -2,7 +2,6 @@ class HomeController < ApplicationController
   
     def index
     @employees = Employee.all
-
     respond_to do |format|
       format.html # index.html.erb
       format.json { render json: @employees }
